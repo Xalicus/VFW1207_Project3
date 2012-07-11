a9649c1882ffa097ad05033497532f9dfdbf95b2
Xalicus <Xalicus@fullsail.edu>
2012-07-11 13:04:25 -0700

    Trying to add more functions for this week.

--- js/main.js
+++ js/main.js
@@ -104,6 +104,10 @@ window.addEventListener("DOMContentLoaded", function(){
 	// My getData function
 	var getData = function() {
 		toggleControls("on");
+		if(localStorage.length === 0) {
+			alert("There is no data in Local Storage.");
+		};
+		
 		// This is supposed to write data from Local Storage back to the browser.
 		var makeDiv = document.createElement("div");
 		makeDiv.setAttribute("id", "items");
@@ -114,6 +118,7 @@ window.addEventListener("DOMContentLoaded", function(){
 		gebi("items").style.display = "block";
 		for (var i=0, len=localStorage.length; i<len; i++) {
 			var makeLi = document.createElement("li");
+			var linksLi = document.createElement("li");
 			makeList.appendChild(makeLi);
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
@@ -126,16 +131,44 @@ window.addEventListener("DOMContentLoaded", function(){
 				makeSubList.appendChild(makeSubLi);
 				var optSubText = object[n][0] + " " + object[n][1];
 				makeSubLi.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
 			};
+			// Create the edit and delete buttons/link for each item in local storage.
+			makeItemLinks(localStorage.key(i));
 		};
 	};
 	
-	// My Clear Data Function
-/*	var clearDataStorage = function() {
-		clear; localStorage.clear();
-		return false;
+/*	// My Make Item Function
+	// Create the edit and delete links for each stored item when displayed.
+	function makeItemLinks(key) {
+		// add edit single item link
+		var editLink = document.createElement("a");
+		var editLink.href = "#";
+		editLink.key = key;
+		var editText = "Edit Pet";
+		editLink.addEventListener("click", editItem);
+		editLink.innerHTML = editText;
+		linksLi.appendChild(editLink);
+		
+		var deleteLink = document.createElement("a");
+		deleteLink.href = "#";
+		deleteLink.key = key;
+		
 	};
+*/
 	
+	// My Clear Data Function
+	var clearDataStorage = function() {
+		if(localStorage.length === 0) {
+			alert("No Kool Pets have been added to the PokeDex.");
+		} else {
+			localStorage.clear();
+			alert("All Kool Pets have been set free!");
+			window.location.reload();
+			return false;
+		};
+	};
+/*	
 	// My Show Array Function
 	var showArray = function() {
 		// Supposed to show something here.
@@ -166,9 +199,9 @@ window.addEventListener("DOMContentLoaded", function(){
 	saveData.addEventListener("click", submit);
 	var showData = gebi("showData");
 	showData.addEventListener("click", getData);
-/*	var clearLink = gebi("clear");	
+	var clearLink = gebi("clear");	
 	clearLink.addEventListener("click", clearDataStorage);
-*/
+
 	
 });
 
